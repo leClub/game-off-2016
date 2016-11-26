a90f3928463da490411d9d412a247ab2bbaf5b68
Clément Ribé <Clment Rib>
2016-11-26 23:06:31 +0100

    [add] Camera behaviour

--- physics2D/Assets/scripts/PlayerController.cs
+++ physics2D/Assets/scripts/PlayerController.cs
@@ -10,12 +10,17 @@ public class PlayerController : MonoBehaviour
     Vector2 anchor;
     bool anchored = false;
     SpringJoint2D spring;
+    Vector3 cameraTargetPos;
+
+    private Vector3 refref = Vector3.zero;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         spring = GetComponent<SpringJoint2D>();
         rb.velocity = new Vector2(10f, 0f);
+
+        cameraTargetPos = transform.position;
     }
 
     void Update()
@@ -42,6 +47,8 @@ public class PlayerController : MonoBehaviour
 
         if (anchored)
         {
+            cameraTargetPos = new Vector3(anchor.x, anchor.y, -10);
+
             float rotationAngle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) - Mathf.PI / 2;
             float x = anchor.x + Mathf.Cos(rotationAngle) * 5;
             float y = anchor.y + Mathf.Sin(rotationAngle) * 5;
@@ -54,51 +61,66 @@ public class PlayerController : MonoBehaviour
                 anchored = false;
             }
         }
+        else
+        {
+            cameraTargetPos = new Vector3(pos.x, pos.y, -10);
+        }
 
         // constant velocity
         Vector3 vel = transform.position + new Vector3(rb.velocity.x, rb.velocity.y, 0);
         rb.velocity = Vector3.ClampMagnitude(rb.velocity * 9999, 15);
         Debug.DrawLine(vel, transform.position, Color.cyan);
+
+        mainCamera.transform.position = Vector3.SmoothDamp(mainCamera.transform.position, cameraTargetPos + new Vector3(rb.velocity.x, rb.velocity.y, 0f), ref refref, 0.4f);
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("enter");
-        anchored = true;
-        anchor = other.transform.position;
+        if (other.tag == "Orbit") {
+            anchored = true;
+            anchor = other.transform.position;
 
-        spring.connectedAnchor = new Vector2(anchor.x, anchor.y);
+            spring.connectedAnchor = new Vector2(anchor.x, anchor.y);
 
-        Vector2 pos = new Vector2(transform.position.x, transform.position.y);
-        Vector2 dir = anchor - pos;
-        dir.Normalize();
-        Vector2 A = new Vector2(anchor.x - dir.y, anchor.y + dir.x);
-        Vector2 B = new Vector2(anchor.x + dir.y, anchor.y - dir.x);
+            Vector2 pos = new Vector2(transform.position.x, transform.position.y);
+            Vector2 dir = anchor - pos;
+            dir.Normalize();
+            Vector2 A = new Vector2(anchor.x - dir.y, anchor.y + dir.x);
+            Vector2 B = new Vector2(anchor.x + dir.y, anchor.y - dir.x);
 
-        //Debug.DrawLine(anchor, A, Color.green, 100);
-        //Debug.DrawLine(anchor, B, Color.red, 100);
+            //Debug.DrawLine(anchor, A, Color.green, 100);
+            //Debug.DrawLine(anchor, B, Color.red, 100);
 
-        //Debug.DrawLine(pos, A, Color.yellow, 100);
-        //Debug.DrawLine(pos, B, Color.yellow, 100);
-        //Debug.DrawLine(pos, anchor, Color.yellow, 100);
+            //Debug.DrawLine(pos, A, Color.yellow, 100);
+            //Debug.DrawLine(pos, B, Color.yellow, 100);
+            //Debug.DrawLine(pos, anchor, Color.yellow, 100);
 
-        float velAngle = Mathf.Atan2(rb.velocity.y, rb.velocity.x);
-        Vector2 posA = A - pos;
-        float posAAngle = Mathf.Atan2(posA.y, posA.x);
-        Vector2 posB = B - pos;
-        float posBAngle = Mathf.Atan2(posB.y, posB.x);
-        Vector2 posAnchor = anchor - pos;
-        float posAnchorAngle = Mathf.Atan2(posAnchor.y, posAnchor.x);
+            float velAngle = Mathf.Atan2(rb.velocity.y, rb.velocity.x);
+            Vector2 posA = A - pos;
+            float posAAngle = Mathf.Atan2(posA.y, posA.x);
+            Vector2 posB = B - pos;
+            float posBAngle = Mathf.Atan2(posB.y, posB.x);
+            Vector2 posAnchor = anchor - pos;
+            float posAnchorAngle = Mathf.Atan2(posAnchor.y, posAnchor.x);
 
-        float[] dif = new float[] { Mathf.Abs(velAngle - posAAngle), Mathf.Abs(velAngle - posBAngle), Mathf.Abs(velAngle - posAnchorAngle) };
-        if (Mathf.Min(dif) == Mathf.Abs(velAngle - posAnchorAngle))
+            float[] dif = new float[] { Mathf.Abs(velAngle - posAAngle), Mathf.Abs(velAngle - posBAngle), Mathf.Abs(velAngle - posAnchorAngle) };
+            if (Mathf.Min(dif) == Mathf.Abs(velAngle - posAnchorAngle))
+            {
+                Debug.Log("Going to crash");
+            }
+            else
+            {
+                isAnchorable = true;
+            }
+        }
+        else if ( other.tag == "Planet" )
         {
-            Debug.Log("crash");
             rb.velocity = Vector3.zero;
+            Debug.Log("BADABOOM !");
         }
         else
         {
-            isAnchorable = true;
+            Debug.Log("??");
         }
     }
 
