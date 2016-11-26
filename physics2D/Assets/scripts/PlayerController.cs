58781fce77e20e6c0bc1dc138174b32a8ae84e22
Clément Ribé <Clment Rib>
2016-11-26 22:02:37 +0100

    merge scene

--- physics2D/Assets/scripts/PlayerController.cs
+++ physics2D/Assets/scripts/PlayerController.cs
@@ -6,10 +6,8 @@ public class PlayerController : MonoBehaviour
     public Camera mainCamera;
     Rigidbody2D rb;
 
-    Vector2 velocity = new Vector2(10f, 0f);
-
     bool isAnchorable = false;
-    Vector3 anchor;
+    Vector2 anchor;
     bool anchored = false;
     SpringJoint2D spring;
 
@@ -17,7 +15,7 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         spring = GetComponent<SpringJoint2D>();
-        rb.velocity = velocity;
+        rb.velocity = new Vector2(10f, 0f);
         anchor = transform.position;
     }
 
@@ -33,11 +31,22 @@ public class PlayerController : MonoBehaviour
         //Debug.DrawLine(vel, transform.position, Color.blue);
         //rb.velocity.Set(transform.right.x * 10f, transform.right.y);
 
+        if (isAnchorable)
+        {
+            //Debug.DrawLine(anchor, pos, Color.red);
+            if (Input.GetKey("space"))
+            {
+                spring.enabled = true;
+                anchored = true;
+                isAnchorable = false;
+            }
+        }
+
         if (anchored)
         {
-            Debug.DrawLine(anchor, pos, Color.red);
+            //Debug.DrawLine(anchor, pos, Color.blue);
 
-            if (Input.GetKey("space"))
+            if (!Input.GetKey("space"))
             {
                 spring.enabled = false;
                 anchored = false;
@@ -64,8 +73,38 @@ public class PlayerController : MonoBehaviour
         anchored = true;
         anchor = other.transform.position;
 
-        spring.enabled = true;
         spring.connectedAnchor = new Vector2(anchor.x, anchor.y);
+
+        Vector2 pos = new Vector2(transform.position.x, transform.position.y);
+        Vector2 dir = anchor - pos;
+        dir.Normalize();
+        Vector2 A = new Vector2(anchor.x - dir.y, anchor.y + dir.x);
+        Vector2 B = new Vector2(anchor.x + dir.y, anchor.y - dir.x);
+
+        Debug.DrawLine(anchor, A, Color.green, 100);
+        Debug.DrawLine(anchor, B, Color.red, 100);
+
+        Debug.DrawLine(pos, A, Color.yellow, 100);
+        Debug.DrawLine(pos, B, Color.yellow, 100);
+        Debug.DrawLine(pos, anchor, Color.yellow, 100);
+
+        float velAngle = Mathf.Atan2(rb.velocity.y, rb.velocity.x);
+        Vector2 posA = A - pos;
+        float posAAngle = Mathf.Atan2(posA.y, posA.x);
+        Vector2 posB = B - pos;
+        float posBAngle = Mathf.Atan2(posB.y, posB.x);
+        Vector2 posAnchor = anchor - pos;
+        float posAnchorAngle = Mathf.Atan2(posAnchor.y, posAnchor.x);
+
+        float[] dif = new float[] { Mathf.Abs(velAngle - posAAngle), Mathf.Abs(velAngle - posBAngle), Mathf.Abs(velAngle - posAnchorAngle) };
+        if (Mathf.Min(dif) == Mathf.Abs(velAngle - posAnchorAngle))
+        {
+            Debug.Log("crash");
+        }
+        else
+        {
+            isAnchorable = true;
+        }
     }
 
     void OnTriggerStay2D(Collider2D other)
@@ -75,6 +114,6 @@ public class PlayerController : MonoBehaviour
 
     void OnTriggerExit2D(Collider2D other)
     {
-        //Debug.Log("exit");
+        isAnchorable = false;
     }
 }
