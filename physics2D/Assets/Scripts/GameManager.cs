2c7e00e530113bc15b8a1a5d9b4915a3a06045a3
Clément Ribé <clement@pixxid.fr>
2017-02-15 18:17:06 +0100

    [add] Add game manager structure and timer countdown

---
+++ physics2D/Assets/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	[System.NonSerialized]
+	public int time, scrore, userXP;
+	public string missionResolution;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Debug.Log (missionResolution);
+		if (missionResolution == "FAIL") {
+			Debug.Log("Back home");
+			SceneManager.LoadScene("Home", LoadSceneMode.Single);
+		}
+	}
+
+	public int Time {
+		get {
+			return time;
+		}
+		set {
+			time = value;
+		}
+	}
+
+	public int Scrore {
+		get {
+			return scrore;
+		}
+		set {
+			scrore = value;
+		}
+	}
+
+	public int UserXP {
+		get {
+			return userXP;
+		}
+		set {
+			userXP = value;
+		}
+	}
+
+	public string MissionResolution {
+		get {
+			return missionResolution;
+		}
+		set {
+			missionResolution = value;
+		}
+	}
+}
---
+++ physics2D/Assets/Scripts/MissionManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MissionManager : MonoBehaviour {
+
+	GameManager gameManager;
+	public Camera mainCamera;
+
+	public Text timerText;
+
+	public float maxTimeInSeconds;
+
+	private string timeRemaining;
+	private float timeInSeconds, minutes, seconds;
+
+	private string missionStatus;
+
+	// Use this for initialization
+	void Start () {
+		timerText = GameObject.Find("Timer").GetComponent<Text>();	
+		timeInSeconds = maxTimeInSeconds;
+
+		gameManager = mainCamera.GetComponent<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DisplayTimer ();
+
+		if (timeInSeconds < 1) {
+			missionStatus = "FAIL";
+			gameManager.MissionResolution = missionStatus;
+		}
+	}
+
+	private void DisplayTimer () {
+		timeInSeconds -= Time.deltaTime;
+
+		int minutes = Mathf.FloorToInt(timeInSeconds / 60F);
+		int seconds = Mathf.FloorToInt(timeInSeconds - minutes * 60);
+		string timeRemaining = string.Format("{0:0}:{1:00}", minutes, seconds);
+
+		timerText.text = timeRemaining;
+	}
+}
